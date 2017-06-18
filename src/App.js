87392d724ed105b522658533e6b5c9d85f6da60c
Charles ⚡ <charlespeters42@gmail.com>
2017-06-17 23:50:31 -0700

    loading and empty state

--- src/App.js
+++ src/App.js
@@ -1,80 +1,55 @@
 // import 'typeface-roboto-mono'
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Block } from 'glamor-jsxstyle'
-import { EditorState } from 'draft-js'
 import Editor from './components/Editor/slate'
 import Edit from './EditEditor'
-import EditorView from './components/EditorView'
 import Header from './components/Header'
 import Main from './Main'
 import NoMatch from './NoMatch'
+// import EditorView from './components/EditorView'
 
-function handleResponse(response) {
-  if (response.ok) {
-    return response.json()
-  } else {
-    let error = new Error(response.statusText)
-    error.response = response
-    throw error
-  }
-}
-
+// function handleResponse(response) {
+//   if (response.ok) {
+//     return response.json()
+//   } else {
+//     let error = new Error(response.statusText)
+//     error.response = response
+//     throw error
+//   }
+// }
 
 class App extends React.Component {
   state = {
-    editorState: EditorState.createEmpty(),
-    postAdded: false,
-    fakeNewTitle: '',
-    posts: []
+    posts: [],
+    loaded: false
   }
 
 
   componentWillMount () {
     fetch('/posts')
       .then(res => res.json())
-      .then(data => this.setState({ posts: data }))
-  }
-
-
-  addNew = () => {
-    fetch('/posts', {
-      method: 'post',
-      body: JSON.stringify({
-        title: this.state.fakeNewTitle,
-        id: this.state.posts.length + 1,
-        body: this.state.editorState,
-        author: 'charlespeters'
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(handleResponse)
-    .then(data => this.setState({ postAdded: true }))
+      .then(data => this.setState({ posts: data, loaded: true }))
   }
 
   render () {
-    const { editorState, fakeNewTitle, posts, postAdded } = this.state
-    const NEditor = () => (
-      <EditorView
-        title={fakeNewTitle}
-        onTitleChange={e => this.setState({ fakeNewTitle: e })}
-        addNew={this.addNew}
-        editorState={editorState}
-        onChange={editorState => this.setState({ editorState })}
-      />
-    )
+    // const NEditor = () => (
+    //   <EditorView
+    //     title={fakeNewTitle}
+    //     onTitleChange={e => this.setState({ fakeNewTitle: e })}
+    //     addNew={this.addNew}
+    //     editorState={editorState}
+    //     onChange={editorState => this.setState({ editorState })}
+    //   />
+    // )
     return (
       <Router>
-        <Block fontFamily='Operator Mono'>
+        <Block fontFamily='Operator Mono' height='calc(100% - 82px)'>
           <Header name='Re:Downwrite Web' />
-          <hr />
           <Switch>
-            <Route exact path='/' render={() => <Main posts={posts} />} />
+            <Route exact path='/' render={() => <Main {...this.state} />} />
             <Route exact path='/editor' component={Editor} />
-            <Route path='/new' render={
-              () => postAdded ? <Redirect /> : <NEditor />
-            } />
+            <Route path='/new' component={Editor} />
             <Route path='/edit/:id' component={Edit} />
             <Route component={NoMatch} />
           </Switch>
--- src/Main.js
+++ src/Main.js
@@ -1,31 +1,49 @@
-import 'typeface-hind'
 import React from 'react'
-import uuid from 'uuid/v4'
 import Card from './components/Card'
+import Loading from './components/Loading'
 import { css } from 'glamor'
 import { Block, Flex } from 'glamor-jsxstyle'
 
-let itemSty = css({
-  width: '100%',
-  marginBottom: 24,
-  maxWidth: 400
-})
+const s = {
+  main: css({
+    paddingTop: 16,
+    marginBottom: 24,
+    fontWeight: 400
+  }),
+  title: css({
+    fontSize: 40,
+    marginBottom: 24
+  }),
+  item: css({
+    marginBottom: 24,
+    width: '100%',
+    '@media (min-width:31.25rem)': { width: '50%' },
+    '@media (min-width: 48rem)': { width: `${100/3}%` },
+    '@media (min-width: 57.75rem)': { width: '25%' }
+  })
+}
 
-let mainTitle = css({
-  paddingTop: 16,
-  marginBottom: 24,
-  fontSize: 40,
-  fontWeight: 400
-})
-
-const MainView = ({ posts }) =>
-  <Block fontFamily='Hind' padding={16}>
-    <h1 className={css(mainTitle)}>Posts</h1>
-    {posts.length > 0 ? (
-        <Flex component='ul' justifyContent='space-between' flexWrap='wrap' listStyle='none inside'>
-          {posts.map(p => <Card tag='li' className={css(itemSty)} key={uuid()} {...p} />)}
-        </Flex>
-      ) : <h2>No Posts</h2>}
+const List = ({ posts }) => (
+  <Block>
+    <h1 className={css(s.title)}>Posts</h1>
+    <Flex component='ul' flexWrap='wrap' margin='0 -20px' listStyle='none inside'>
+      {posts.map(p => <Card className={css(s.item)} key={p.id} {...p} />)}
+    </Flex>
   </Block>
+)
 
-export default MainView
+const Empty = () => (
+  <Block>
+    <h2>No Posts</h2>
+    <p>Maybe you should start writing?</p>
+  </Block>
+)
+
+export default ({ posts, loaded }) =>
+  <Block padding={16} height='100%'>
+    {
+      loaded
+      ? (posts.length > 0 ? <List posts={posts} /> : <Empty />)
+      : <Loading size={100} />
+    }
+  </Block>
