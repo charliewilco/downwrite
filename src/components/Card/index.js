4c33304922c5cc974f5de71e8b0a71fc0071dd91
Charles ⚡ <charlespeters42@gmail.com>
2017-06-17 23:52:44 -0700

    organizing styles and adding excerpt

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,34 +3,42 @@ import { Flex, Block } from 'glamor-jsxstyle'
 import { Link } from 'react-router-dom'
 import { css } from 'glamor'
 
-let titleStyle = css({
-  fontSize: 24,
-  fontWeight: 400
-})
-
-let bodyStyle = css({
-  fontSize: `small`,
-  opacity: 0.5
-})
-
-let cardStyle = {
-  borderLeftWidth: 8,
-  borderLeftStyle: 'solid',
-  borderColor: `var(--color-1)`,
-  boxShadow: '0 0 4px rgba(0,0,0,.07), 0 4px 8px rgba(0,0,0,.14)',
-  backgroundColor: 'white'
+const s = {
+  title: css({
+    fontSize: 24,
+    fontWeight: 600
+  }),
+  body: css({
+    fontSize: `small`,
+    opacity: 0.875,
+    padding: 16
+  }),
+  card: css({
+    borderLeftWidth: 8,
+    borderLeftStyle: 'solid',
+    borderColor: `var(--color-1)`,
+    boxShadow: '0 0 2px rgba(0,0,0,.07), 0 2px 4px rgba(0,0,0,.12)',
+    backgroundColor: 'white'
+  }),
+  meta: css({
+    opacity: .5
+  })
 }
 
-const Card = ({ tag, title, id, author, className, ...args }) => (
-  <Block component={tag} className={css(className, cardStyle)} padding={20} {...args}>
-    <Flex borderBottom='1px solid #f2f2f2' paddingBottom={20} marginBottom={16} justifyContent='space-between' alignItems='center'>
-      <div>
-        <h2 className={titleStyle}>{title} - {id}</h2>
-        <small children={author} />
-      </div>
-      <Link to={`edit/${id}`}>Edit</Link>
-    </Flex>
-    {/* <p className={bodyStyle}>{body}</p> */}
+const Card = ({ title, id, content, author, className, ...props }) => (
+  <Block component='li' padding='0 20px' className={className}>
+    <Block className={css(s.card)} {...props}>
+      <Flex borderBottom='1px solid #f2f2f2' padding={16} justifyContent='space-between' alignItems='flex-start'>
+        <div>
+          <h2 className={s.title}>{title}</h2>
+          <small className={s.meta}>
+            {author} - {id}
+          </small>
+        </div>
+        <Link to={`edit/${id}`}>Edit</Link>
+      </Flex>
+      {content && <p className={s.body}>{content.slice(0, 90)}...</p>}
+    </Block>
   </Block>
 )
 
