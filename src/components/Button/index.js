58c83dfa34ca054561a1ac6d7bf1b36ef091fd75
Charles ⚡ <charlespeters42@gmail.com>
2017-06-17 23:50:46 -0700

    yellow button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,24 +5,24 @@ const BUTTON_SIZE = 62.5
 
 const btn = css({
   display: 'block',
-  backgroundColor: '#2584A5',
+  fontSize: 16,
+  backgroundColor: 'var(--color-6)',
   transition: 'background-color 250ms ease-in-out',
   color: 'white',
   border: 0,
   padding: 16,
   borderRadius: '50%',
-  maxHeight: BUTTON_SIZE,
+  height: BUTTON_SIZE,
   width: BUTTON_SIZE,
-  height: '100%',
   boxShadow: '0 0 4px rgba(0,0,0,.07), 0 4px 8px rgba(0,0,0,.14)',
   ':hover': {
-    backgroundColor: 'var(--color-2)'
+    backgroundColor: 'var(--color-7)'
   }
 })
 
 const positionedSty = css({
   position: 'absolute',
-  left: -32,
+  right: -32,
   top: -32
 })
 
