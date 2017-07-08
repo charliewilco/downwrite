5f17b0bcbb065fdd4d7b91ec77faeb6517f92455
Charles ⚡ <charlespeters42@gmail.com>
2017-07-08 16:06:09 -0700

    removing redux boilerplate

--- src/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
-import posts from './reducers/posts'
-
-export default combineReducers({ posts, router: routerReducer })
