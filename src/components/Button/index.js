1283d97742972bf9ca79f6703f7ff8adb9f4e367
Charles ⚡ <charlespeters42@gmail.com>
2017-07-17 18:48:19 -0700

    updating overflow issue

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -11,8 +11,8 @@ const btn = css({
   border: 0,
   padding: 16,
   borderRadius: '50%',
-  height: 62.5,
-  width: 62.5,
+  height: 64,
+  width: 64,
   boxShadow: '0 0 4px rgba(0,0,0,.07), 0 4px 8px rgba(0,0,0,.14)',
   ':hover': {
     backgroundColor: 'var(--color-7)'
@@ -20,9 +20,14 @@ const btn = css({
 })
 
 const positionedSty = css({
-  position: 'absolute',
-  right: -32,
-  top: -32
+  position: 'fixed',
+  top: 64,
+  right: 32,
+  '@media only screen and (min-width: 48rem)': {
+    position: 'absolute',
+    right: -32,
+    top: -32,
+  }
 }, btn)
 
 const Button = ({ onClick, positioned, type, ...args }) => (
--- src/components/Typescale/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Block } from 'jsx-style'
-
-const F = (props) =>
-  props.6
-  <Block {...props} />
-
-export default F
