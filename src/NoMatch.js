920daf2656009a74d2fb81a0507ee2856c494b7e
Charles ⚡ <charlespeters42@gmail.com>
2017-08-05 18:37:28 -0700

    adding component for 404

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Wrapper from './components/Wrapper'
 import { InlineBlock as In } from 'glamor/jsxstyle'
 
+const Title = ({name}) => (
+  <In
+    component='h2'
+    marginBottom={32}
+    fontWeight={100}
+    fontSize={84}
+    lineHeight={1}
+    children={name}
+  />
+)
+
 export default ({ location }) => (
   <div>
     <Wrapper textAlign='center' padding={`8rem 1rem`}>
-      <In component='h2' marginBottom={32} fontWeight={100} fontSize={84} lineHeight={1}>
-        404
-      </In>
-      <p>
-        Sorry but {location.pathname} didn’t match any pages
-      </p>
+      <Title name='404' />
+      <p>Sorry but {location.pathname} didn’t match any pages</p>
     </Wrapper>
   </div>
 )
