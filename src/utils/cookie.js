40dade6684e8316975d89a42cfe1c5a986eb3c32
Charles ⚡ <charlespeters42@gmail.com>
2017-11-07 20:31:28 -0800

    setting up new auth workflow

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,7 +1,19 @@
 // @flow
-import { Cookies } from 'react-cookie'
-
+import Cookies from 'universal-cookie'
 const oatmeal: typeof Cookies = new Cookies()
 
 export const TOKEN: string = oatmeal.get('token')
 export const USER_ID: string = oatmeal.get('id')
+
+
+export const signOut: Function = () => {
+	oatmeal.remove('token')
+	oatmeal.remove('id')
+}
+
+export default oatmeal
+
+export const intialState = {
+	token: TOKEN,
+	user: USER_ID
+}
