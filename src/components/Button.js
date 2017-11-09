a15fe244babeab640df86d520658915fdf6eb641
Charles ⚡ <charlespeters42@gmail.com>
2017-11-08 16:06:41 -0800

    updating editor UI

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,10 +9,11 @@ const btn = css({
 	backgroundColor: 'var(--color-6)',
 	transition: 'background-color 250ms ease-in-out',
 	color: '#282828',
+	fontWeight: 700,
 	border: 0,
 	paddingTop: 4,
-	paddingLeft: 12,
-	paddingRight: 12,
+	paddingLeft: 18,
+	paddingRight: 18,
 	paddingBottom: 4,
 	borderRadius: 4,
 	boxShadow: '0 0 4px rgba(0,0,0,.037), 0 4px 8px rgba(0,0,0,.07)',
@@ -21,27 +22,12 @@ const btn = css({
 	}
 })
 
-const positionedSty = css(
-	{
-		position: 'fixed',
-		top: 64,
-		right: 32,
-		'@media only screen and (min-width: 48rem)': {
-			position: 'absolute',
-			right: -16,
-			top: -32
-		}
-	},
-	btn
-)
-
 type Props = {
-	positioned: Boolean,
 	args: Object
 }
 
-const Button = ({ positioned, ...args }: Props) => (
-	<button className={css(positioned ? positionedSty : btn)} {...args} />
+const Button = ({ ...args }: Props) => (
+	<button className={css(btn)} {...args} />
 )
 
 export default Button
