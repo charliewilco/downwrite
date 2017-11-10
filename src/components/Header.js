e18ce4c700129736ab8f4b0eb21edc875d1bae5a
Charles ⚡ <charlespeters42@gmail.com>
2017-11-09 16:26:43 -0800

    fixing eslint and flow errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Nav from './Nav'
 import Logo from './Logo'
-import Aux from './Aux'
-import Toggle from './Toggle'
 import { css } from 'glamor'
 import { Flex, Row } from 'glamor/jsxstyle'
 
