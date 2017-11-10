967b38b5fc6e58df3ed3cf20571db17c7b966ccc
Charles ⚡ <charlespeters42@gmail.com>
2017-11-09 16:05:25 -0800

    fixes icon swapping bug

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -25,13 +25,13 @@ const ModalCloseIcon = () => (
 		<g id="Canvas" transform="translate(-1561 -730)">
 			<g id="Close">
 				<g id="Combined Shape">
-					<use xlinkHref="#path0_fill" transform="translate(1561 730)" fill="#4382A1" />
+					<use xlinkHref="#modalClose" transform="translate(1561 730)" fill="#4382A1" />
 				</g>
 			</g>
 		</g>
 		<defs>
 			<path
-				id="path0_fill"
+				id="modalClose"
 				fillRule="evenodd"
 				d="M 6.70588 6L 12 0.705882L 11.2941 0L 6 5.29412L 0.705882 2.83725e-14L 0 0.705882L 5.29412 6L 0 11.2941L 0.705882 12L 6 6.70588L 11.2941 12L 12 11.2941L 6.70588 6Z"
 			/>
