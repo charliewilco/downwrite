15665acf24f4a350a234c949f0d660ac1d5ec4e7
Charles ⚡ <charlespeters42@gmail.com>
2017-11-10 19:58:12 -0800

    toggling Nav

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -5,13 +5,17 @@ export default class extends Component {
 		defaultOpen: false
 	}
 
+	static displayName = 'ToggleFaC'
+
 	state = {
 		open: this.props.defaultOpen
 	}
 
+	close = () => this.setState({ open: false })
+
 	toggle = () => this.setState(({ open }) => ({ open: !open }))
 
 	render() {
-		return this.props.children(this.state.open, this.toggle)
+		return this.props.children(this.state.open, this.toggle, this.close)
 	}
 }
