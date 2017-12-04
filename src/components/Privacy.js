6cf406191842fefcd62ca6316b8af5d678163b55
Charles ⚡ <charlespeters42@gmail.com>
2017-12-03 23:55:13 -0800

    new checkbox

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Block } from 'glamor/jsxstyle'
+import Check from './Checkbox'
 
 const Privacy = ({ title, publicStatus, onChange }) => (
 	<Block width="100%" maxWidth={512} marginBottom={16}>
@@ -11,14 +12,15 @@ const Privacy = ({ title, publicStatus, onChange }) => (
 			a URL to see the content.
 		</small>
 		<Block>
-			<Block component="label">
-				<input
-					type="checkbox"
-					checked={publicStatus}
-					onChange={onChange}
-					value={publicStatus}
-				/>
-				<span>
+			<Block component="label" fontSize={12} padding="16px 0">
+				<Check checked={publicStatus} onChange={onChange} value={publicStatus} />
+				<span
+					style={{
+						marginLeft: 16,
+						display: 'inline-block',
+						verticalAlign: 'middle',
+						lineHeight: 1.1
+					}}>
 					<em>{title}</em> is <span>{publicStatus ? 'Public' : 'Private'}</span>
 				</span>
 			</Block>
