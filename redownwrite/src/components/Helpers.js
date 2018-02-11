7f09b6835c00fbf948330e5f97269d6575a6e134
Charles ⚡ <charlespeters42@gmail.com>
2018-02-11 14:04:15 -0800

    adding chevron

--- redownwrite/src/components/Helpers.js
+++ redownwrite/src/components/Helpers.js
@@ -5,6 +5,25 @@ import Media from 'react-media'
 
 // Open Button Should be a Chevron
 
+const Chevron = ({ open }) => (
+  <svg
+    className="Chevron"
+    viewBox="0 0 16 16"
+    width="16"
+    height="16"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}>
+    <path
+      d={
+        open
+          ? 'M1.0606601717798212 11 L8 4.060660171779821 L14.939339828220179 11'
+          : 'M1.0606601717798212 5 L8 11.939339828220179 L14.939339828220179 5'
+      }
+    />
+  </svg>
+)
+
 export default class extends Component {
   static displayName = 'HelperToolbar'
   render() {
@@ -22,7 +41,12 @@ export default class extends Component {
                   marginBottom={16}
                   justifyContent={children ? 'space-between' : 'flex-end'}
                   padding={!match && 8}>
-                  {!match && children && <button onClick={toggle}>Open</button>}{' '}
+                  {!match &&
+                    children && (
+                      <button style={{ outline: 0, border: 0 }} onClick={toggle}>
+                        <Chevron open={open} />
+                      </button>
+                    )}{' '}
                   {render && render()}
                 </Flex>
                 {(match || open) && (
