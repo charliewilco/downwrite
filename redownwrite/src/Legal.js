dadd2c22167ec893d42527e3d9d01c5e2ca2d287
Charles ⚡ <charlespeters42@gmail.com>
2018-02-18 17:49:59 -0800

    adding privacy policy

--- redownwrite/src/Legal.js
+++ redownwrite/src/Legal.js
@@ -1,31 +1,134 @@
 import React from 'react'
 import { Content } from './components'
 
-let tldr = {
-  terms: `
-    You're accepting that any public entry containing liablous, profane or racist,
-    queerphobic, xenophobic or sexist content has the potential to be revoked
-    and set back to private at my descrition.
-  `,
-  privacy: `
-    Any content, albeit your personal information or an entry, is private until you
-    opt out of making the entry private. I do not have the intention of selling,
-    repackaging for sale, any of your content.
-  `
-}
-
 let legalInfo = `
+## Terms of Service
 
+> **TLDR**: You're accepting that any public entry containing libelous, profane or racist,
+queerphobic, xenophobic or sexist content has the potential to be revoked
+and set back to private at my discretion.
 
-  ## Terms of Service
+### 1\. Terms
 
-  > **TLDR**: ${tldr.terms.trim()}
+By accessing the website at [http://downwrite.us](http://downwrite.us), you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.
 
+### 2\. Use License
 
-  ## Privacy Policy
+1\.  Permission is granted to temporarily download one copy of the materials (information or software) on Downwrite's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
 
-  > **TLDR**: ${tldr.privacy.trim()}
+- modify or copy the materials;
+- use the materials for any commercial purpose, or for any public display (commercial or non-commercial);
+- attempt to decompile or reverse engineer any software contained on Downwrite's website;
+- remove any copyright or other proprietary notations from the materials; or
+- transfer the materials to another person or "mirror" the materials on any other server.
 
+2\.  This license shall automatically terminate if you violate any of these restrictions and may be terminated by Downwrite at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+
+### 3\. Disclaimer
+
+1. The materials on Downwrite's website are provided on an 'as is' basis. Downwrite makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
+2. Further, Downwrite does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.
+
+### 4\. Limitations
+
+In no event shall Downwrite or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Downwrite's website, even if Downwrite or a Downwrite authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+
+### 5\. Accuracy of materials
+
+The materials appearing on Downwrite website could include technical, typographical, or photographic errors. Downwrite does not warrant that any of the materials on its website are accurate, complete or current. Downwrite may make changes to the materials contained on its website at any time without notice. However Downwrite does not make any commitment to update the materials.
+
+### 6\. Links
+
+Downwrite has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Downwrite of the site. Use of any such linked website is at the user's own risk.
+
+### 7\. Modifications
+
+Downwrite may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.
+
+### 8\. Governing Law
+
+These terms and conditions are governed by and construed in accordance with the laws of WA and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.
+
+## Privacy Policy
+
+> **TLDR**: Any content, albeit your personal information or an entry, is private until you
+opt out of making the entry private. I do not have the intention of selling,
+repackaging for sale, any of your content. And obviously this site uses cookies and
+disabling them is a bad idea.
+
+Your privacy is important to us.
+
+As a web-based service provider, we require some personal information in order to provide a service to you.
+
+However, we don’t ask for this information unless we truly need it.
+
+It is Downwrite's policy to respect your privacy regarding any information we may collect from you across our website, [http://downwrite.us](http://downwrite.us).
+
+### Personal Information
+
+We may ask you for personal information, such as:
+
+* Your name
+* Email address
+* Social media profile/s, and
+* Contact details
+
+This information is used for the purposes of:
+
+* providing you with products and services
+* providing you with information about our products and services
+* managing and improving our products, services, quality control, administration, communication and internal processes
+* personalising and customising your experience with us
+* verifying your identity
+* granting you access to your account with us
+* investigating any enquiries or complaints submitted by or about you; and/or
+* complying with regulatory or legal obligations
+
+You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.
+
+We do not share your personal information with third-parties, except where required by law, to protect our own rights, or to provide a service to you.
+
+We will only retain personal information for as long as necessary to provide you with a service.
+
+### Business Data
+
+Our respect for your privacy extends to the data you input into our website over the normal course of using our services. We will not share your data with third-parties, except where required by law, to protect our own rights, or to provide a service to you. We may, however, disclose anonymised and aggregated versions of this information for business, marketing or public relations purposes. We will only retain your data for as long as necessary to provide you with a service.
+
+### Cookies
+
+We use "cookies" to collect information about you and your activity across our site. A cookie is a small piece of data that our website stores on your computer, and accesses each time you visit so we can understand how you use our site and serve you content based on preferences you have specified.
+
+If you do not wish to accept cookies from us, you should instruct your browser to refuse cookies from our website, with the understanding that we may be unable to provide you with some of your desired service without them. This policy covers only the use of cookies between your computer and our website; it does not cover the use of cookies by any advertisers.
+
+### Third-Party Services
+
+We employ third-party services for our:
+
+* Website analytics
+* Customer relationship management, and
+* Advertising and remarketing
+
+These third parties have access to selected personal information only to perform specific tasks on our behalf or to improve the relevance of information presented to you. We review the privacy policies of all our third-party providers before enlisting their services to ensure their practices align with ours.
+
+### Security
+
+We take security seriously, and do what we can within commercially acceptable means to protect your personal information from loss or theft, as well as unauthorized access, disclosure, copying, use or modification. That said, we advise that no method of electronic transmission or storage is 100% secure, and cannot guarantee the absolute security of your data.
+
+### Links to Other Sites
+
+Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot assume responsibility for their treatment of your personal information. This privacy policy only covers our website and privacy practices.
+
+### Children’s Privacy
+
+We do not knowingly collect or store personal information from children (visitors under the age of 13). If you believe your child has provided us with personal information, we encourage you to contact us immediately, and we will do our best to delete the data as quickly as possible.
+
+### Changes to our Privacy Policy
+
+At our discretion, we may change our privacy policy from time to time. Any changes will be reflected here, so we encourage you to visit this page regularly. We will also notify our registered users of updates to our policy. Your continued use of this site after any changes to this policy will be regarded as acceptance of our practices around privacy and personal information.
+
+### Business Transfers
+
+If we or our assets are acquired, or in the unlikely event that we go out of business or enter bankruptcy, we would include user information among our assets transferred to or acquired by a third party. You acknowledge that such transfers may occur, and that any parties who acquire us may continue to use your personal information according to this policy.
 `
 
 export default () => <Content title="Legal Nonsense" content={legalInfo} />
