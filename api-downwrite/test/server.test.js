f05b7b1fbb6c4feb18f179d85f0dc50ff3980606
Charles ⚡ <charlespeters42@gmail.com>
2018-02-19 02:01:10 -0800

    adding test cases

--- api-downwrite/test/server.test.js
+++ api-downwrite/test/server.test.js
@@ -3,32 +3,33 @@ const server = require('../src/server')
 const mock = jest.fn()
 const req = {}
 
-const requestDefaults = {
+const defaultGet = {
   method: 'GET',
   url: '/posts',
   payload: {}
 }
 
+const previewReq = {
+  method: 'GET',
+  url: '/posts/preview/aa3dd293-2a0e-478c-81e7-a0b9733e8b'
+}
+
 describe('Server Endpoints Perform', () => {
   it('GET | status code is 400', async () => {
-    const request = Object.assign({}, requestDefaults)
-
+    const request = Object.assign({}, defaultGet)
     const response = await server.inject(request)
 
     expect(response.statusCode).isEqual(400)
   })
 
   it('GET | PREVIEW | status code is 200 on a public post', async () => {
-    const request = Object.assign(
-      {},
-      {
-        method: 'GET',
-        url: '/posts/preview/aa3dd293-2a0e-478c-81e7-a0b9733e8b'
-      }
-    )
-
+    const request = Object.assign({}, previewReq)
     const response = await server.inject(request)
 
     expect(response.statusCode).isEqual(200)
   })
+
+  xit('POST')
+
+  xit('USER')
 })
