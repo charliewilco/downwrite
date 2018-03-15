b610c1c328fbd23854a3e74855cf435a62abae63
Charles ⚡ <charlespeters42@gmail.com>
2018-03-15 01:25:34 -0700

    STATELESS FUNCTION

--- redownwrite/src/App.js
+++ redownwrite/src/App.js
@@ -10,18 +10,20 @@ import OfflineContainer from './Offline'
 import { Shell, UIFlash, Loading, Logger, Offline } from './components/'
 import { PrivateRoute } from './CustomRoutes'
 
-type AppProps = {
-  authed: boolean,
-  token: string,
-  user: string,
-  name: string,
-  signIn: Function,
-  signOut: Function
-}
-
 type LoaderOpts = {
   loader: Function
 }
+
+const Ldx = (opts: LoaderOpts) => Loadable(Object.assign({ loading: Loading }, opts))
+
+const New = Ldx({ loader: () => import('./New') })
+const Edit = Ldx({ loader: () => import('./Edit') })
+const Home = Ldx({ loader: () => import('./Home') })
+const Dashboard = Ldx({ loader: () => import('./Dashboard') })
+const NotFound = Ldx({ loader: () => import('./NoMatch') })
+const SignOut = Ldx({ loader: () => import('./SignOut') })
+const Preview = Ldx({ loader: () => import('./Preview') })
+const Legal = Ldx({ loader: () => import('./Legal') })
 
 const ErrorMessage = () => (
   <Subscribe to={[ErrorContainer]}>
@@ -31,91 +33,50 @@ const ErrorMessage = () => (
   </Subscribe>
 )
 
-const Ldx = (opts: LoaderOpts) => Loadable(Object.assign({ loading: Loading }, opts))
-
-const New = Ldx({
-  loader: () => import('./New')
-})
-
-const Edit = Ldx({
-  loader: () => import('./Edit')
-})
-
-const Home = Ldx({
-  loader: () => import('./Home')
-})
-
-const Dashboard = Ldx({
-  loader: () => import('./Dashboard')
-})
-
-const NotFound = Ldx({
-  loader: () => import('./NoMatch')
-})
-
-const SignOut = Ldx({
-  loader: () => import('./SignOut')
-})
-
-const Preview = Ldx({
-  loader: () => import('./Preview')
-})
-
-const Legal = Ldx({
-  loader: () => import('./Legal')
-})
-
-export default class extends React.Component<AppProps, void> {
-  static displayName = 'App'
-
-  render() {
-    return (
-      <Subscribe to={[AuthContainer, OfflineContainer]}>
-        {(auth, offline) => (
-          <React.Fragment>
-            <Offline onChange={offline.handleChange} />
-            <Logger value={[auth, offline]} />
-            <Helmet title="Downwrite" />
-            <Router>
-              <Shell auth={auth} renderErrors={() => <ErrorMessage />}>
-                {closeNav => (
-                  <Switch>
-                    <Route
-                      exact
-                      path="/"
-                      render={(props: Object) =>
-                        auth.state.authed === true ? (
-                          <Dashboard
-                            token={auth.state.token}
-                            user={auth.state.user}
-                            closeNav={closeNav}
-                            {...props}
-                          />
-                        ) : (
-                          <Home {...props} signIn={auth.signIn} signOut={auth.signOut} />
-                        )
-                      }
-                    />
-
-                    <PrivateRoute path="/new" component={New} />
-                    <PrivateRoute path="/:id/edit" component={Edit} />
-                    <Route exact path="/:id/preview" component={Preview} />
-                    <Route
-                      exact
-                      path="/signout"
-                      render={(props: Object) => (
-                        <SignOut toggleNav={closeNav} signOut={auth.signOut} />
-                      )}
-                    />
-                    <Route path="/legal" component={Legal} />
-                    <Route component={NotFound} />
-                  </Switch>
-                )}
-              </Shell>
-            </Router>
-          </React.Fragment>
-        )}
-      </Subscribe>
-    )
-  }
-}
+export default () => (
+  <Subscribe to={[AuthContainer, OfflineContainer]}>
+    {(auth, offline) => (
+      <React.Fragment>
+        <Offline onChange={offline.handleChange} />
+        <Logger value={[auth, offline]} />
+        <Helmet title="Downwrite" />
+        <Router>
+          <Shell auth={auth} renderErrors={() => <ErrorMessage />}>
+            {closeNav => (
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={(props: Object) =>
+                    auth.state.authed === true ? (
+                      <Dashboard
+                        token={auth.state.token}
+                        user={auth.state.user}
+                        closeNav={closeNav}
+                        {...props}
+                      />
+                    ) : (
+                      <Home {...props} signIn={auth.signIn} signOut={auth.signOut} />
+                    )
+                  }
+                />
+                <PrivateRoute path="/new" component={New} />
+                <PrivateRoute path="/:id/edit" component={Edit} />
+                <Route exact path="/:id/preview" component={Preview} />
+                <Route
+                  exact
+                  path="/signout"
+                  render={(props: Object) => (
+                    <SignOut toggleNav={closeNav} signOut={auth.signOut} />
+                  )}
+                />
+                <Route path="/legal" component={Legal} />
+                <Route component={NotFound} />
+              </Switch>
+            )}
+          </Shell>
+        </Router>
+      </React.Fragment>
+    )}
+  </Subscribe>
+)
