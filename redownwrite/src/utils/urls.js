a53f0b69fced25c653ce941574b54f666eb4d13e
Charles ⚡ <charlespeters42@gmail.com>
2018-03-14 23:56:33 -0700

    resolves https://github.com/charliewilco/downwrite/issues/64

--- redownwrite/src/utils/urls.js
+++ redownwrite/src/utils/urls.js
@@ -2,18 +2,17 @@
 
 import __IS_DEV__ from './dev'
 
-export const POST_ENDPOINT: string = __IS_DEV__
-  ? 'http://localhost:4411/posts'
-  : 'https://api.downwrite.us/posts'
+const DEV_URL = 'http://localhost:4411'
+const PROD_URL = 'https://api.downwrite.us'
+
+export const POST_ENDPOINT: string = __IS_DEV__ ? `${DEV_URL}/posts` : `${PROD_URL}/posts`
 
 export const PREVIEW_ENDPOINT: string = __IS_DEV__
-  ? 'http://localhost:4411/posts/preview'
-  : 'https://api.downwrite.us/posts/preview'
+  ? `${DEV_URL}/posts/preview`
+  : `${PROD_URL}/posts/preview`
 
-export const USER_ENDPOINT: string = __IS_DEV__
-  ? 'http://localhost:4411/users'
-  : 'https://api.downwrite.us/users'
+export const USER_ENDPOINT: string = __IS_DEV__ ? `${DEV_URL}/users` : `${PROD_URL}/users`
 
 export const AUTH_ENDPOINT: string = __IS_DEV__
-  ? 'http://localhost:4411/users/authenticate'
-  : 'https://api.downwrite.us/users/authenticate'
+  ? `${DEV_URL}/users/authenticate`
+  : `${PROD_URL}/users/authenticate`
