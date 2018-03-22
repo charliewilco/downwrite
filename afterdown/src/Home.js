5f99f923997c4c9a7490f7fe51aaf21b97c8e108
Charles ⚡ <charlespeters42@gmail.com>
2018-03-22 00:23:56 -0700

    basic after.js implementation

---
+++ afterdown/src/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link'
+import 'universal-fetch'
+import './Home.css'
+
+export default class Home extends Component {
+  static async getInitialProps({ req, res, match, history, location, ...ctx }) {
+    const response = await fetch('https://api.github.com/users/charliewilco/repos')
+    const posts = await response.json()
+
+    return { posts }
+  }
+
+  constructor(props) {
+    super()
+
+    props.prefetch()
+  }
+
+  render() {
+    const { posts } = this.props
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <h2>Welcome to After.js</h2>
+        </div>
+        <ul className="HomeList">
+          {posts.length > 0 ? (
+            posts.map((post, i) => (
+              <li key={i}>
+                <Link to={`/${post.full_name}`}>{post.full_name}</Link>
+              </li>
+            ))
+          ) : (
+            <h1>Loading</h1>
+          )}
+        </ul>
+        <Link to="/about">About -></Link>
+      </div>
+    )
+  }
+}
