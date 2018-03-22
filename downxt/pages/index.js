ca4216a32eb25646a5591a292ca728f965444f1a
Charles ⚡ <charlespeters42@gmail.com>
2018-03-22 00:23:25 -0700

    basic next.js

--- downxt/pages/index.js
+++ downxt/pages/index.js
@@ -1,5 +1,26 @@
-export default () => (
-  <div>
-    <h1>Hello from Express</h1>
-  </div>
-)
+import React, { Component } from 'react'
+import 'isomorphic-fetch'
+
+export default class Index extends Component {
+  static async getInitialProps() {
+    const resp = await fetch(`http://localhost:4411/posts`, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNWEwNjI3MzY5NmFiZjQwNjJmYzE5OGJhIiwibmFtZSI6InRlc3QiLCJpYXQiOjE1MjE2ODI3NDIsImV4cCI6MTUzNzIzNDc0Mn0.TIgfOVxgI2jOH9tDFiYzmVjWyhZvsh7J5qbfwNXLXe0`
+      }
+    })
+
+    const posts = await resp.json()
+
+    return {
+      posts
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
