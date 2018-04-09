db7d7088e614ef71e1a8225ef9d6044f24b6f9f9
Charles ⚡ <charlespeters42@gmail.com>
2018-04-08 20:53:19 -0700

    migrating a few ideas to redownwrite to make more accessible to SSR

--- redownwrite/src/App.js
+++ redownwrite/src/App.js
@@ -1,24 +1,10 @@
 // @flow
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider, Subscribe } from 'unstated'
 import Helmet from 'react-helmet'
-import Loadable from 'react-loadable'
 import { AuthContainer, OfflineContainer, ErrorContainer } from './containers'
 import * as DW from './components'
-import { PrivateRoute } from './CustomRoutes'
-
-const Ldx = (opts: { loader: Function }) =>
-  Loadable(Object.assign({}, { loading: DW.Loading }, opts))
-
-const New = Ldx({ loader: () => import('./New') })
-const Edit = Ldx({ loader: () => import('./Edit') })
-const Home = Ldx({ loader: () => import('./Home') })
-const Dashboard = Ldx({ loader: () => import('./Dashboard') })
-const NotFound = Ldx({ loader: () => import('./NoMatch') })
-const SignOut = Ldx({ loader: () => import('./SignOut') })
-const Preview = Ldx({ loader: () => import('./Preview') })
-const Legal = Ldx({ loader: () => import('./Legal') })
+import Routes from './Routes'
 
 const UIErrorMessage = ({ content, type, onClose }) =>
   content.length > 0 ? (
@@ -35,42 +21,11 @@ const Downwrite = () => (
           <DW.Offline onChange={offline.handleChange} />
           <DW.Logger value={[auth, offline]} />
           <Helmet title="Downwrite" />
-          <Router>
-            <DW.Shell
-              auth={auth}
-              renderErrors={() => <UIErrorMessage {...err.state} onClose={err.clearFlash} />}>
-              {closeNav => (
-                <Switch>
-                  <Route
-                    exact
-                    path="/"
-                    render={(props: Object) =>
-                      auth.state.authed === true ? (
-                        <Dashboard
-                          token={auth.state.token}
-                          user={auth.state.user}
-                          closeNav={closeNav}
-                          {...props}
-                        />
-                      ) : (
-                        <Home {...props} signIn={auth.signIn} signOut={auth.signOut} />
-                      )
-                    }
-                  />
-                  <PrivateRoute path="/new" component={New} />
-                  <PrivateRoute path="/:id/edit" component={Edit} />
-                  <Route exact path="/:id/preview" component={Preview} />
-                  <Route
-                    exact
-                    path="/signout"
-                    render={() => <SignOut toggleNav={closeNav} signOut={auth.signOut} />}
-                  />
-                  <Route path="/legal" component={Legal} />
-                  <Route component={NotFound} />
-                </Switch>
-              )}
-            </DW.Shell>
-          </Router>
+          <DW.Shell
+            auth={auth}
+            renderErrors={() => <UIErrorMessage {...err.state} onClose={err.clearFlash} />}>
+            {closeNav => <Routes auth={auth} closeNav={closeNav} />}
+          </DW.Shell>
         </React.Fragment>
       )}
     </Subscribe>
--- redownwrite/src/index.js
+++ redownwrite/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Downwrite from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { loadCSS } from 'fg-loadcss'
 import registerServiceWorker from './registerServiceWorker'
@@ -10,7 +11,13 @@ let PREVIEW_FONTS = 'https://cloud.typography.com/7107912/7471392/css/fonts.css'
 let container = document.getElementById('root')
 let styleEntry = document.getElementById('loadcss')
 
-render(<Downwrite />, container)
+const App = () => (
+  <BrowserRouter>
+    <Downwrite />
+  </BrowserRouter>
+)
+
+render(<App />, container)
 
 loadCSS(PREVIEW_FONTS, styleEntry)
 registerServiceWorker()
