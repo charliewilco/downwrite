23ed672b296304c020fde6e3b1d1c1d4caf65e32
Charles ⚡ <charlespeters42@gmail.com>
2018-04-14 11:19:42 -0700

    updating deps, updating `componentWillMount` and prepping for SSR

--- redownwrite/src/CustomRoutes.js
+++ redownwrite/src/CustomRoutes.js
@@ -5,6 +5,7 @@ import { Subscribe } from 'unstated'
 import { Route, Redirect, type Location } from 'react-router-dom'
 
 type CustomRouteProps = {
+  defaultComponent: ?React.Element<any>,
   component: React.ElementType,
   args: Array<number>
 }
@@ -29,6 +30,53 @@ export const PrivateRoute: React.ElementType = ({
             <Redirect to={{ pathname: '/', state: { from: props.location } }} />
           )
         }
+      />
+    )}
+  </Subscribe>
+)
+
+export const IndexRoute: React.ElementType = ({
+  component: Component,
+  defaultComponent: DefaultComponent
+}: CustomRouteProps) => (
+  <Subscribe to={[AuthContainer, ErrorContainer]}>
+    {(auth, err) => (
+      <Route
+        exact
+        path="/"
+        render={(props: Object) =>
+          auth.state.authed === true ? (
+            <Component
+              setError={err.setError}
+              token={auth.state.token}
+              user={auth.state.user}
+              {...props}
+            />
+          ) : (
+            <DefaultComponent {...props} signIn={auth.signIn} signOut={auth.signOut} />
+          )
+        }
+      />
+    )}
+  </Subscribe>
+)
+
+export const PublicRoute: React.ElementType = ({
+  component: Component,
+  ...args
+}: CustomRouteProps) => (
+  <Subscribe to={[AuthContainer, ErrorContainer]}>
+    {(auth, err) => (
+      <Route
+        {...args}
+        render={(props: { location: Location }) => (
+          <Component
+            setError={err.setError}
+            token={auth.state.token}
+            user={auth.state.user}
+            {...props}
+          />
+        )}
       />
     )}
   </Subscribe>
