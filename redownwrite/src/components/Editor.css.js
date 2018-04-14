276b2feff041b413015c1e234a5fc5a76e138059
Charles ⚡ <charlespeters42@gmail.com>
2018-04-14 13:03:29 -0700

    adding background to the code block in editor

--- redownwrite/src/components/Editor.css.js
+++ redownwrite/src/components/Editor.css.js
@@ -80,6 +80,12 @@ export default injectGlobal`
     font-size: 100%;
   }
 
+  .RichEditor-editor pre {
+    background: #f4f5f5;
+    padding: .75rem;
+    font-size: 13px;
+  }
+
   .public-DraftStyleDefault-orderedListItem > *,
   .public-DraftStyleDefault-unorderedListItem > * {
     display: inline;
