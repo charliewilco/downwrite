18005879884691697c9323be7544f66da292fadb
Charles ⚡ <charlespeters42@gmail.com>
2018-04-22 20:55:29 -0700

    preparing for data-fetching

---
+++ redownwrite/src/routeMap.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import NewEditor from './New'
+import PostEditor from './Edit'
+import Home from './Home'
+import Dashboard from './Dashboard'
+import NotFound from './NoMatch'
+import SignOut from './SignOut'
+import Preview from './Preview'
+import Legal from './Legal'
+
+import { IndexRoute, PublicRoute, PrivateRoute } from './CustomRoutes'
+
+export const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Dashboard,
+    defaultComponent: Home,
+    home: true
+  },
+  {
+    path: '/new',
+    component: NewEditor,
+    private: true
+  },
+  {
+    path: '/:id/edit',
+    component: PostEditor,
+    private: true
+  },
+  {
+    path: '/:id/preview',
+    component: Preview
+  },
+  {
+    path: '/legal',
+    component: Legal
+  }
+]
+
+export const chooseRoute = route =>
+  route.private ? PrivateRoute : route.home ? IndexRoute : PublicRoute
+
+export const chooseComponent = (route, authed) =>
+  !authed && route.defaultComponent ? route.defaultComponent : route.component
+
+export const findRoute = (routes, authed, initialData) => {
+  return routes.map((route, i) => {
+    const Route = chooseRoute(route)
+    const Cx = chooseComponent(route, authed)
+
+    const SSRCx = () => <Cx {...initialData} />
+
+    return <Route key={i} {...route} component={SSRCx} />
+  })
+}
--- redownwrite/src/server.js
+++ redownwrite/src/server.js
@@ -1,19 +1,21 @@
 import './shims'
-import React, { Fragment } from 'react'
+import React from 'react'
 import express from 'express'
 import cookieMiddleware from 'universal-cookie-express'
 import isEmpty from 'lodash/isEmpty'
 import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom'
+import { StaticRouter, matchPath } from 'react-router-dom'
 import Loadable from 'react-loadable'
 import { getBundles } from 'react-loadable/webpack'
 import { ServerStyleSheet } from 'styled-components'
+import 'universal-fetch'
+
 import Downwrite from './App'
 import stats from '../build/react-loadable.json'
 import NoMatch from './NoMatch'
 import renderer from './utils/renderer'
+import { routes, findRoute } from './routeMap'
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 const app = express()
 
 app.disable('x-powered-by')
@@ -23,37 +25,49 @@ app.use(cookieMiddleware())
 app.get('/*', (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-  const cookie = req.universalCookies.get('DW_TOKEN')
+  const activeRx = routes.find(route => matchPath(req.url, route))
 
+  const token = req.universalCookies.get('DW_TOKEN')
+  const authed = !isEmpty(token)
   let modules = []
   let context = {}
-  let serverContext = {
-    state: { token: cookie, authed: !isEmpty(cookie) },
-    signIn: () => {},
-    signOut: () => {}
+
+  const defaultInitialDataParams = {
+    url: req.url,
+    query: matchPath(req.url, activeRx.path)
   }
 
-  const sheet = new ServerStyleSheet()
+  console.log(defaultInitialDataParams)
 
-  let markup = renderToString(
-    sheet.collectStyles(
-      <Loadable.Capture report={moduleName => modules.push(moduleName)}>
-        <StaticRouter location={req.url} context={context}>
-          <Downwrite serverContext={serverContext} />
-        </StaticRouter>
-      </Loadable.Capture>
-    )
-  )
-
-  const styleTags = sheet.getStyleTags()
-
-  const bundles = getBundles(stats, modules)
-  const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'))
+  const requestInitialData =
+    activeRx.component.getInitialData &&
+    activeRx.component.getInitialData(defaultInitialDataParams, token)
 
   try {
-    let html = renderer(styleTags, markup, chunks, serverContext)
+    Promise.resolve(requestInitialData)
+      .then(initialData => {
+        const sheet = new ServerStyleSheet()
 
-    res.send(html)
+        let markup = renderToString(
+          sheet.collectStyles(
+            <Loadable.Capture report={moduleName => modules.push(moduleName)}>
+              <StaticRouter location={req.url} context={context}>
+                <Downwrite serverContext={{ token, authed }}>
+                  {findRoute(routes, authed, initialData)}
+                </Downwrite>
+              </StaticRouter>
+            </Loadable.Capture>
+          )
+        )
+
+        const styleTags = sheet.getStyleTags()
+
+        const bundles = getBundles(stats, modules)
+        const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'))
+        let html = renderer(styleTags, markup, chunks, serverContext)
+        return res.send(html)
+      })
+      .catch(err => console.error(err))
   } catch (error) {
     let errSheet = new ServerStyleSheet()
     let errorContainer = renderToString(
