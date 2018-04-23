2a841df11b9a5446c6220c88cbcf0b7974bc9ee0
Charles ⚡ <charlespeters42@gmail.com>
2018-04-22 21:37:44 -0700

    removing offline unstated.

--- redownwrite/src/containers/index.js
+++ redownwrite/src/containers/index.js
@@ -1,4 +1,3 @@
 export { default as AuthContainer } from './Auth'
 export { default as ErrorContainer } from './Errors'
-export { default as OfflineContainer } from './Offline'
 export { default as LocalDraftsContainer } from './LocalDrafts'
