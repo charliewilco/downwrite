b5e2ee6c7d8d84a389661e45dcf85404ddcfa74d
Charles ⚡ <charlespeters42@gmail.com>
2018-09-15 16:12:45 -0700

    consolidating builds

--- redownwrite/next.config.js
+++ redownwrite/next.config.js
@@ -1,7 +1,7 @@
-const withOffline = require('next-offline')
-const withManifest = require('next-manifest')
-const withCSS = require('@zeit/next-css')
-const withTypescript = require('@zeit/next-typescript')
+const withOffline = require('next-offline');
+const withManifest = require('next-manifest');
+const withCSS = require('@zeit/next-css');
+const withTypescript = require('@zeit/next-typescript');
 
 const manifest = {
   dir: 'ltr',
@@ -11,22 +11,22 @@ const manifest = {
   description: 'A place to write',
   icons: [
     {
-      src: 'icon192-beta.png',
+      src: 'icon192.png',
       sizes: '192x192',
       type: 'image/png'
     },
     {
-      src: 'icon256-beta.png',
+      src: 'icon256.png',
       sizes: '256x256',
       type: 'image/png'
     },
     {
-      src: 'icon512-beta.png',
+      src: 'icon512.png',
       sizes: '512x512',
       type: 'image/png'
     },
     {
-      src: 'icon1024-beta.png',
+      src: 'icon1024.png',
       sizes: '1024x1024',
       type: 'image/png'
     }
@@ -35,6 +35,6 @@ const manifest = {
   background_color: '#4FA5C2',
   start_url: '/?utm_source=homescreen',
   display: 'standalone'
-}
+};
 
-module.exports = withTypescript(withCSS(withManifest(withOffline({ manifest }))))
+module.exports = withTypescript(withCSS(withManifest(withOffline({ manifest }))));
