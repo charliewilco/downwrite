f94b6b1cad9d2fd6f1354bd3aa2fc639bc647a99
Charles ⚡ <charlespeters42@gmail.com>
2019-01-11 05:52:39 -0800

    adding more tests

--- client/__tests__/export.test.tsx
+++ client/__tests__/export.test.tsx
@@ -1,7 +1,14 @@
 import Export from "../components/export";
 import { render } from "react-testing-library";
+import { createEditorState } from "./config/createMocks";
 
-const { container } = render(<Export />);
+const { container } = render(
+  <Export
+    title="Testing Patience"
+    editorState={createEditorState("Something, Something")}
+    date={new Date()}
+  />
+);
 
 describe("<Export />", () => {
   it("renders", () => {
---
+++ client/__tests__/schema.test.ts
@@ -0,0 +1,28 @@
+import * as Validations from "../utils/validations";
+
+describe("Validations", () => {
+  it("Local Settings", async () => {
+    const result = await Validations.LocalSettingsSchema.isValid({
+      fontFamily: "Fira Code",
+      fileExtension: "doc"
+    });
+    expect(result).toBeFalsy();
+  });
+
+  it("User Settings", async () => {
+    const result = await Validations.UserSettingsSchema.isValid({
+      email: "red.com",
+      user: "somehello"
+    });
+
+    expect(result).toBeFalsy();
+  });
+
+  it("Login", async () => {
+    const result = await Validations.LoginFormSchema.isValid({
+      user: "hello",
+      password: "not hello"
+    });
+    expect(result).toBeTruthy();
+  });
+});
