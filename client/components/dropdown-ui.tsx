0a6ec093042d435dee280c42c9b12cf1d5a936dc
Charles ⚡ <charlespeters42@gmail.com>
2019-04-15 13:22:27 -0700

    Adding Dark Mode to Menu

--- client/components/dropdown-ui.tsx
+++ client/components/dropdown-ui.tsx
@@ -4,9 +4,12 @@ import * as Reach from "@reach/menu-button";
 import { NavIcon, SignoutIcon } from "./icons";
 import User from "./user";
 import { AuthContext, IAuthContext } from "./auth";
+import { NightModeContext, INightModeContext } from "./night-mode";
 
 export default function DropdownUI() {
   const auth = React.useContext<IAuthContext>(AuthContext);
+  const darkMode = React.useContext<INightModeContext>(NightModeContext);
+
   return (
     <Reach.Menu>
       <Reach.MenuButton className="DropdownMenuButton">
@@ -23,6 +26,9 @@ export default function DropdownUI() {
         <Reach.MenuLink onClick={() => Router.push("/settings")} component="a">
           Settings
         </Reach.MenuLink>
+        <Reach.MenuItem onSelect={darkMode.action.onChange}>
+          {darkMode.night ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </Reach.MenuItem>
         <Reach.MenuItem onSelect={auth.signOut}>
           <SignoutIcon className="mid" /> <span className="mid">Sign Out</span>
         </Reach.MenuItem>
