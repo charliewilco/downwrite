78289b9a45768ebe5eff16fa05d397cec5ad2e13
Charles ⚡ <charlespeters42@gmail.com>
2019-08-23 17:04:24 -0700

    passing tests

--- client/tests/url.test.ts
+++ client/tests/url.test.ts
@@ -1,17 +1,19 @@
-import { createURL } from "../utils/urls";
+import { URLEndpoints } from "../utils/urls";
 import { Endpoints } from "../utils/api";
 
 describe("urls", () => {
   it("updates a url", () => {
-    expect(createURL(Endpoints.POST_ENDPOINT, "localhost:5000")).toContain("http");
-    expect(createURL(Endpoints.POST_ENDPOINT, "localhost:5000")).not.toContain(
-      "//api"
+    expect(URLEndpoints.create(Endpoints.POST_ENDPOINT, "localhost:5000")).toContain(
+      "http"
     );
     expect(
-      createURL(Endpoints.POST_ENDPOINT, "downwrite-9ivohrqa3.now.sh")
+      URLEndpoints.create(Endpoints.POST_ENDPOINT, "localhost:5000")
+    ).not.toContain("//api");
+    expect(
+      URLEndpoints.create(Endpoints.POST_ENDPOINT, "downwrite-9ivohrqa3.now.sh")
     ).toContain("http");
     expect(
-      createURL(Endpoints.POST_ENDPOINT, "downwrite-9ivohrqa3.now.sh")
+      URLEndpoints.create(Endpoints.POST_ENDPOINT, "downwrite-9ivohrqa3.now.sh")
     ).not.toContain("//api");
   });
 });
