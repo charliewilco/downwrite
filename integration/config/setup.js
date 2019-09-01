1eed8cd4eb0f7648cacfa97e0e5d8a08b5ef0d34
Charles ⚡ <charlespeters42@gmail.com>
2019-08-31 23:17:48 -0700

    removing integration test suite

--- integration/config/setup.js
+++ integration/config/setup.js
@@ -4,7 +4,7 @@ module.exports = async function globalSetup() {
   await setupDevServer({
     command: `yarn workspaces run dev`,
     launchTimeout: 50000,
-    port: 3000
+    port: 7000
   });
   // Your global setup
 };
