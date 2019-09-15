b94bae3e1609ea5a39952ea79f388a78c9c21551
Charles ⚡ <charlespeters42@gmail.com>
2019-09-14 23:26:17 -0700

    removing @zeit/next-typescript

--- client/next.config.js
+++ client/next.config.js
@@ -1,70 +1,67 @@
 const withOffline = require("next-offline");
 const withManifest = require("next-manifest");
 const withCSS = require("@zeit/next-css");
-const withTypescript = require("@zeit/next-typescript");
 const withMDX = require("@zeit/next-mdx")({
   extension: /\.mdx?$/
 });
 
 module.exports = withCSS(
-  withTypescript(
-    withMDX(
-      withOffline({
-        target: "serverless",
-        workboxOpts: {
-          runtimeCaching: [
-            {
-              urlPattern: /.png$/,
-              handler: "CacheFirst"
-            },
-            {
-              urlPattern: /api/,
-              handler: "NetworkFirst",
-              options: {
-                cacheableResponse: {
-                  statuses: [0, 200],
-                  headers: {
-                    "x-test": "true"
-                  }
+  withMDX(
+    withOffline({
+      target: "serverless",
+      workboxOpts: {
+        runtimeCaching: [
+          {
+            urlPattern: /.png$/,
+            handler: "CacheFirst"
+          },
+          {
+            urlPattern: /api/,
+            handler: "NetworkFirst",
+            options: {
+              cacheableResponse: {
+                statuses: [0, 200],
+                headers: {
+                  "x-test": "true"
                 }
               }
             }
-          ]
-        },
-        manifest: {
-          dir: "ltr",
-          lang: "en",
-          short_name: "Dwnxt",
-          name: "Downwrite Next",
-          description: "A place to write",
-          icons: [
-            {
-              src: "icon192.png",
-              sizes: "192x192",
-              type: "image/png"
-            },
-            {
-              src: "icon256.png",
-              sizes: "256x256",
-              type: "image/png"
-            },
-            {
-              src: "icon512.png",
-              sizes: "512x512",
-              type: "image/png"
-            },
-            {
-              src: "icon1024.png",
-              sizes: "1024x1024",
-              type: "image/png"
-            }
-          ],
-          theme_color: "#4FA5C2",
-          background_color: "#4FA5C2",
-          start_url: "/?utm_source=homescreen",
-          display: "standalone"
-        }
-      })
-    )
+          }
+        ]
+      },
+      manifest: {
+        dir: "ltr",
+        lang: "en",
+        short_name: "Dwnxt",
+        name: "Downwrite Next",
+        description: "A place to write",
+        icons: [
+          {
+            src: "icon192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icon256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "icon512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "icon1024.png",
+            sizes: "1024x1024",
+            type: "image/png"
+          }
+        ],
+        theme_color: "#4FA5C2",
+        background_color: "#4FA5C2",
+        start_url: "/?utm_source=homescreen",
+        display: "standalone"
+      }
+    })
   )
 );
