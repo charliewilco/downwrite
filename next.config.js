ca2b8c1bfa79d7e283a17006adc948da73a4dc96
Charles ⚡ <charlespeters42@gmail.com>
2019-09-29 17:49:52 -0700

    mock to fi build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
-  target: "serverless"
+  target: "serverless",
+  webpack(config, { isServer }) {
+    if (isServer) {
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
+        mocks
+      ];
+    }
+
+    return config;
+  }
 });
+
+const mocks = {
+  bcrypt: "bcrypt",
+  jsonwebtoken: "jsonwebtoken",
+  "node-pre-gyp": "node-pre-gyp",
+  mongoose: "mongoose",
+  "apollo-server-micro": "apollo-server-micro",
+  "aws-sdk": "aws-sdk"
+};
