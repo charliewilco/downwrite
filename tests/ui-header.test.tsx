454beb4938280359024fbe7d1c84fab4237506a0
Charles ⚡ <charlespeters42@gmail.com>
2019-12-29 03:38:58 -0800

    not using react-test-renderer

--- tests/ui-header.test.tsx
+++ tests/ui-header.test.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import TestRenderer from "react-test-renderer";
-
+import { render } from "@testing-library/react";
 import { UIHeader } from "../components/header";
 import { LinkProps } from "next/link";
 import { MockAuthProvider } from "../components/auth";
-import { act } from "react-dom/test-utils";
 
 jest.mock("universal-cookie", () => {
   return class Cookie {};
@@ -18,41 +15,18 @@ jest.mock("next/link", () => {
   ));
 });
 
-const app = (
-  <MockAuthProvider>
-    <UIHeader />
-  </MockAuthProvider>
-);
-
-let container: HTMLDivElement;
-
-const testMock = TestRenderer.create(app);
-
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-// NOTE: test broken by upgrading @testing-library
 describe("Header Component", () => {
   it("contains application name", () => {
-    act(() => {
-      ReactDOM.render(app, container);
-    });
+    const utils = render(
+      <MockAuthProvider>
+        <UIHeader />
+      </MockAuthProvider>
+    );
 
-    const title = container.querySelector("h1");
-    const header = container.querySelector("header");
+    const title = utils.container.querySelector("h1");
+    const header = utils.container.querySelector("header");
 
     expect(title.textContent).toBe("Downwrite");
     expect(header).toBeTruthy();
-  });
-
-  it("matches snapshot", () => {
-    expect(testMock.toJSON()).toMatchSnapshot();
   });
 });
