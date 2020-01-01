ba7fd35a343923787eceb22a9ac899ed576f1a41
Charles ⚡ <charlespeters42@gmail.com>
2020-01-01 03:13:45 -0800

    fixing card tests

--- tests/card.test.tsx
+++ tests/card.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { LinkProps } from "next/link";
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, waitForElement } from "@testing-library/react";
 import Card from "../components/card";
 import { createMockPost } from "../utils/create-mocks";
 
@@ -15,33 +15,32 @@ jest.mock("next/link", () => {
   ));
 });
 
-const { container, getByTestId } = render(
-  <Card public={false} {...post} onDelete={mockDelete} />
-);
-
-// NOTE: test broken by upgrading @testing-library
-xdescribe("<Card />", () => {
+describe("<Card />", () => {
   it("contains snippet from content", () => {
-    const snippet = getByTestId("CARD_EXCERPT").textContent;
+    const TestCard = render(<Card public={false} {...post} onDelete={mockDelete} />);
+    const snippet = TestCard.getByTestId("CARD_EXCERPT").textContent;
     expect(snippet.length).toBeLessThanOrEqual(90);
   });
 
   it("contains a title", () => {
+    const TestCard = render(<Card public={false} {...post} onDelete={mockDelete} />);
     expect(post.title).toBe(title);
-    expect(getByTestId("CARD_TITLE")).toHaveTextContent(title);
+    expect(TestCard.getByTestId("CARD_TITLE")).toHaveTextContent(title);
   });
 
-  it("contains delete button", () => {
-    expect(getByTestId("CARD_DELETE_BUTTON")).toBeInTheDocument();
+  it("contains delete button", async () => {
+    const TestCard = render(<Card public={false} {...post} onDelete={mockDelete} />);
+    const deleteButton = await waitForElement(() =>
+      TestCard.getByTestId("CARD_DELETE_BUTTON")
+    );
+    expect(deleteButton).toBeInTheDocument();
+    fireEvent.click(deleteButton);
+
+    expect(mockDelete).toHaveBeenCalled();
   });
 
   it("matches snapshot", () => {
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("should fire a delete", () => {
-    fireEvent.click(getByTestId("CARD_DELETE_BUTTON"));
-
-    expect(mockDelete).toHaveBeenCalled();
+    const TestCard = render(<Card public={false} {...post} onDelete={mockDelete} />);
+    expect(TestCard.container.firstChild).toMatchSnapshot();
   });
 });
