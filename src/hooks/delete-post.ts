90fc633f8dd9ce414ba89e252710e582ff8cdf1f
Charles ⚡ <charlespeters42@gmail.com>
2020-02-16 02:28:40 -0800

    adding generic

--- src/hooks/delete-post.ts
+++ src/hooks/delete-post.ts
@@ -10,7 +10,7 @@ type RemoveFn = (id: string) => Promise<void>;
 export function useRemovePost(): RemoveFn {
   const [deleteEntry] = useRemoveEntryMutation();
 
-  const onConfirmDelete = React.useCallback(
+  const onConfirmDelete = React.useCallback<RemoveFn>(
     async function(id: string): Promise<void> {
       await deleteEntry({
         variables: { id },
