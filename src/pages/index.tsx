e5cb05efb56ec63ae7988b14508698f8f53ea805
Charles ⚡ <charlespeters42@gmail.com>
2020-06-29 17:03:35 -0700

    adding redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useAuthContext } from "../components/auth";
 import { useRouter } from "next/router";
-// import { GetServerSideProps } from "next";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "../lib/cookie-managment";
 // import { initializeApollo } from "../lib/apollo";
 
 export default function IndexPage() {
@@ -14,3 +15,21 @@ export default function IndexPage() {
 
   return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { DW_TOKEN } = parseCookies(context.req);
+
+  if (DW_TOKEN) {
+    return {
+      props: {
+        token: DW_TOKEN,
+        initialApolloState: {}
+      }
+    };
+  } else {
+    context.res.setHeader("location", "/login");
+    context.res.statusCode = 302;
+    context.res.end();
+    return { props: {} };
+  }
+};
