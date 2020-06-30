b93f727b1c925e23613153141858b0ce23a9fb7f
Charles ⚡ <charlespeters42@gmail.com>
2020-06-29 22:47:43 -0700

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,27 @@
 import { useEffect } from "react";
-import { useAuthContext } from "../components/auth";
+import { NormalizedCacheObject } from "@apollo/client";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
-import { GetServerSideProps } from "next";
 import { parseCookies } from "../lib/cookie-managment";
 // import { initializeApollo } from "../lib/apollo";
 
-export default function IndexPage() {
-  const [{ authed }] = useAuthContext();
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace(authed ? "/dashboard" : "/login");
-  }, [router, authed]);
-
-  return null;
+interface IIndexProps {
+  token: string;
+  initialApolloState: NormalizedCacheObject;
+  redirect: string;
 }
+type IndexProps = IIndexProps | {};
 
-export const getServerSideProps: GetServerSideProps = async context => {
+export const getServerSideProps: GetServerSideProps<IndexProps> = async context => {
   const { DW_TOKEN } = parseCookies(context.req);
 
   if (DW_TOKEN) {
+    console.log("TOKEN", DW_TOKEN);
     return {
       props: {
         token: DW_TOKEN,
-        initialApolloState: {}
+        initialApolloState: {},
+        redirect: "/dashboard"
       }
     };
   } else {
@@ -33,3 +31,19 @@ export const getServerSideProps: GetServerSideProps = async context => {
     return { props: {} };
   }
 };
+
+export default function IndexPage(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!!props && props !== {}) {
+      if ((props as IIndexProps).redirect) {
+        router.replace((props as IIndexProps).redirect);
+      }
+    }
+  }, [router, props]);
+
+  return null;
+}
