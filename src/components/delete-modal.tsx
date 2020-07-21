5cd7066f07d5e52d0d7f931243144cb7d3e03074
Charles ⚡ <charlespeters42@gmail.com>
2020-07-21 03:18:36 -0700

    Updating modal

--- src/components/delete-modal.tsx
+++ src/components/delete-modal.tsx
@@ -13,16 +13,18 @@ interface IDeleteModalProps {
 export default function DeleteModal(props: IDeleteModalProps) {
   return (
     <Modal closeUIModal={props.closeModal}>
-      <div className="pt-6 px-2 pb-0">
+      <div className="bg-onyx-900 pt-4 px-2 pb-0">
         <div className="mb-4 p-4">
-          <h6 className="font-black">Delete Post</h6>
+          <h6 className="font-black text-xl">Delete Post</h6>
           <p className="my-4 mx-0 text-base">
             Are you sure you want to delete <b>{quotedTitle(props.title)}</b>?
           </p>
         </div>
         <footer className="border-t flex justify-end py-4 px-0">
           <AltButton onClick={props.onCancelDelete}>Cancel</AltButton>
-          <Button onClick={props.onDelete}>Delete</Button>
+          <Button className="ml-8" onClick={props.onDelete}>
+            Delete
+          </Button>
         </footer>
       </div>
     </Modal>
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -10,7 +10,7 @@ export default function UIModal(props: IModalProps) {
     <DialogOverlay className="overflow-auto fixed z-50 top-0 bottom-0 flex justify-center flex-col items-center w-full p-2 ">
       <DialogContent className="w-full m-auto bg-white h-auto flex relative">
         <button
-          className="absolute m-0 appearance-none p-4"
+          className="absolute m-0 appearance-none p-2 right-0 top-0"
           onClick={props.closeUIModal}>
           <FiX className="" />
         </button>
