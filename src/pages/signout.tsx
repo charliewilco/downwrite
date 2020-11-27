7f20372fb1c87aa2ca03a0f21e4bbc12f3cf4d3b
Charles ⚡ <charlespeters42@gmail.com>
2020-11-26 20:56:23 -0800

    adding more routes

---
+++ src/pages/signout.tsx
@@ -0,0 +1,24 @@
+import { GetServerSideProps, NextPage } from "next";
+import { removeTokenCookie } from "@lib/cookie-managment";
+
+export const getServerSideProps: GetServerSideProps<
+  { token: "" },
+  any
+> = async context => {
+  removeTokenCookie(context.res);
+  context.res.setHeader("location", "/login");
+  context.res.statusCode = 302;
+  context.res.end();
+
+  return {
+    props: {
+      token: ""
+    }
+  };
+};
+
+const SignOut: NextPage = () => {
+  return <h1>Goodbye!</h1>;
+};
+
+export default SignOut;
