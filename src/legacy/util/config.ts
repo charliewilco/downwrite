cb13df0f7de9f835573c3026af51550b0f3bf11c
Charles ⚡ <charlespeters42@gmail.com>
2020-12-08 00:29:24 -0800

    fixing env variable

--- src/legacy/util/config.ts
+++ src/legacy/util/config.ts
@@ -1,6 +1,6 @@
 const Config = {
   key: process.env.SECRET_KEY || "1a9876c4-6642-4b83-838a-9e84ee00646a",
-  dbCreds: `mongodb://${process.env.CURRENT_DB_ADDRESS}`
+  dbCreds: process.env.ATLAS_DB_ADDRESS,
 };
 
 export default Config;
