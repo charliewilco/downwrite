8859a72b83df18e5ed686b2228e9a99ae4dc5114
Charles ⚡ <charlespeters42@gmail.com>
2020-12-25 04:02:58 -0800

    testing puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,11 +2,11 @@ module.exports = {
   server: {
     command: "NO_HTTPS=true yarn start",
     port: 3000,
-    launchTimeout: 45000,
     debug: false
   },
   launch: {
     headless: true,
-    slowMo: 0
+    slowMo: 0,
+    args: ["--no-sandbox"]
   }
 };
--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -3,7 +3,7 @@ const ts = require("ts-jest/jest-preset");
 const puppeteer = require("jest-puppeteer/jest-preset");
 
 module.exports = merge.recursive(ts, puppeteer, {
-  testTimeout: 100000,
+  testTimeout: 10000,
   verbose: true,
   testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.[jt]sx?$"
 });
