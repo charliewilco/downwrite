e7f942e4931f1aed7e63def1a7d766c812521189
Charles ⚡ <charlespeters42@gmail.com>
2020-12-25 02:41:24 -0800

    puppeteer settings

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,10 +3,10 @@ module.exports = {
     command: "NO_HTTPS=true yarn start",
     port: 3000,
     launchTimeout: 45000,
-    debug: true
+    debug: false
   },
   launch: {
-    headless: !!process.env.CI,
-    slowMo: process.env.CI ? 0 : 250
+    headless: true,
+    slowMo: 0
   }
 };
