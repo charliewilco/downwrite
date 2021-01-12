2ffb967a93fab914a8c808f5b54047a1751f710d
Charles ⚡ <charlespeters42@gmail.com>
2021-01-12 00:39:10 -0800

    adding analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
 import { AppProps } from "next/app";
+import Router from "next/router";
 import { ApolloProvider } from "@apollo/client";
+import * as Analytics from "fathom-client";
 import { UIShell } from "@components/ui-shell";
 import { useApollo } from "@lib/apollo";
 import { AppProvider } from "@reducers/app";
 import "../styles.css";
+import { useEffect } from "react";
+
+Router.events.on("routeChangeComplete", () => {
+  Analytics.trackPageview();
+});
 
 export default function CustomAppWrapper({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState);
+
+  useEffect(() => {
+    Analytics.load("FENETBXC", {
+      includedDomains: [
+        "downwrite.us",
+        "alpha.downwrite.us",
+        "beta.downwrite.us",
+        "next.downwrite.us"
+      ]
+    });
+  }, []);
 
   return (
     <ApolloProvider client={client}>
