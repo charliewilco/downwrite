50680b789222c7d8d999cf2c7abf909a5a6888c8
Charles ⚡ <charlespeters42@gmail.com>
2021-01-11 22:57:13 -0800

    updating document references

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+// @ts-check
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true
-});
+const graphqlRule = {
+  test: /\.(graphql|gql)$/,
+  exclude: /node_modules/,
+  loader: "graphql-tag/loader"
+};
+
+const config = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push(graphqlRule);
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    return config;
+  }
+};
+
+module.exports = withBundleAnalyzer(config);
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,5 +1,5 @@
 import { makeExecutableSchema } from "apollo-server-micro";
-import { typeDefs } from "@lib/type-defs";
+import typeDefs from "./schema.graphql";
 import { resolvers } from "@lib/resolvers";
 
 export const schema = makeExecutableSchema({
